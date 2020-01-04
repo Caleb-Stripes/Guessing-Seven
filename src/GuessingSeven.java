2bfc592029b7d6a541825eadcb1074b2c66b7ec0
Caleb-Stripes <calebdstripes@gmail.com>
2020-01-04 16:37:35 -0500

    iteration 3 is sparkly

--- src/GuessingSeven.java
+++ src/GuessingSeven.java
@@ -7,33 +7,37 @@ public class GuessingSeven {
 
 		Scanner input = new Scanner (System.in);
 		System.out.println("Guess a number between 0 and 11.");
-		int guess = input.nextInt();
+		
+		
+		int attempts = 0;
+		
+		while (attempts < 3) {
+		
+			int guess = input.nextInt();	
+		
+		attempts = attempts + 1;
+		 
 		
 		if (guess == -1) {
 			System.out.println("Program terminated.");
 			System.exit(0);
-		}
+		} 
 		
 		if (guess == 0) {
 			System.out.println("You must choose one of the following: \n1, 2, 3, 4, 5, 6, 7, 8, 9 or 10.");
-			int guess2 = input.nextInt();
-			if (guess2 == 7) {
-				System.out.println("Success!");
-			} else {
-				System.out.println("Too bad you lose.");
-			}	
-		} else if (guess != 7) {
-			System.out.println("Guess Again.");
-			int guess2 = input.nextInt();
-			if (guess2 == 7) {
-				System.out.println("Success!");
-			} else {
-				System.out.println("Too bad you lose.");
-			}
+			
+		} else if (guess > 7) {
+			System.out.println("Guess Again. The number is LOWER.");
+		
+		} else if (guess < 7) {
+			System.out.println("Guess Again. The number is HIGHER.");
+		
 		} else {
 			System.out.println("Success!");
+			System.out.println("Program terminated.");
+			System.exit(0);
 		}
-		
+		}
 		input.close();
 	}
 
